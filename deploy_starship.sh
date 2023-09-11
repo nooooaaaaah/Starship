3bf3b19dede63f594f40d2aa0d6b2abe4dcda651
Noah <nspielman@live.com>
2023-09-11 16:08:29 -0600

    /

--- deploy_starship.sh
+++ deploy_starship.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The destination where the starship.toml file is read by Starship
-STARSHIP_CONFIG_DEST="~/.config/Starship"
+STARSHIP_CONFIG_DEST="~/.config/Starship/"
 
 # Check out the latest version of the main branch to the destination directory
 GIT_WORK_TREE="$STARSHIP_CONFIG_DEST" git checkout -f main
